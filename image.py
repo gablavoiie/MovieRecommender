5c09d1abcd87fb4091466aee1eb8fd0e08371894
Rylan Donohoe <rylan.donohoe@outlook.com>
2023-01-29 06:28:30 -0500

    update

--- image.py
+++ image.py
@@ -1,16 +1,14 @@
 import pandas as pd
-
-df3 = pd.read_csv("movie_data.csv")
-
 import requests
 from bs4 import BeautifulSoup
 
+df3 = pd.read_csv("movie_data.csv")
+
+# returns poster image url from movie index
 def image_url(index):
     query = str(df3.iloc[index,2]) + ' poster'
     url = 'https://www.google.com/search?q=' + query + '&tbm=isch'
     content = requests.get(url).content
     soup = BeautifulSoup(content,'lxml')
-    image = soup.find('img')
-    return image.get('src')
-
-print(image_url(0))
+    images = soup.findAll('img')
+    return images[1].get('src')
