2584674c3b4601515e0a0d135ac2a777a30df387
rianadutta <riana.dutta@mail.mcgill.ca>
2023-01-29 06:35:40 -0500

    pic

--- image.py
+++ image.py
@@ -5,8 +5,8 @@ from bs4 import BeautifulSoup
 df3 = pd.read_csv("movie_data.csv")
 
 # returns poster image url from movie index
-def image_url(index):
-    query = str(df3.iloc[index,2]) + ' poster'
+def image_url(title):
+    query = title + ' poster'
     url = 'https://www.google.com/search?q=' + query + '&tbm=isch'
     content = requests.get(url).content
     soup = BeautifulSoup(content,'lxml')
