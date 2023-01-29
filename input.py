327581c37865bc1320deb67436d3d0f4ed6f5c15
rianadutta <riana.dutta@mail.mcgill.ca>
2023-01-29 06:11:48 -0500

    vector

--- input.py
+++ input.py
@@ -1,40 +1,77 @@
 from imdb import Cinemagoer
+from coding import *
+from coherenlp import *
+from vector_score import *
+import pandas
 
 ia = Cinemagoer()
 
+
+df = pandas.read_csv("movie_data.csv")
+
 def take_input(moviedict):
-    if moviedict["simiar_movies"] != "no":
+    moremovies = False
+
+    nlpout = nlp(moviedict["textbox"])
+    res = []
+    '''
+    if moviedict["similar_movies"] != "no":
+        moremovies = True
         movies = moviedict["similar_movies"]
         movlist = movies.split(",")
-        res = []
+        
         vectors = []
+        tags=[]
         for mov in movlist:
             res.append(ia.search_movie(mov)[0]["title"])
-        for movie in res:
             
-            vectors.append([]) # TO DO check if movie in csv file, if so query csv filet to get vector for movie with that title
-    
-    if movie["genres"] != "no":
-        genreforvector = genreencoding(moviedict["genres"]) 
+    '''
+    if moviedict["genres"] != "no":
+        yo = moviedict["genres"]
+        if not nlpout[0] in moviedict["genres"]:
+            if not nlpout[0] == "no":
+                yo = nlpout[0]+","+moviedict["genres"]
+
+        genreforvector = genre_encoding(yo) 
         
-    if movie["decade"] != "no":
-        decadecode = decadecoder(moviedict["decade"].strip())
+    if moviedict["decade"] != "no":
+        decadecode = decade_coder(moviedict["decade"].strip())
+    else:
+        decadecode = 5
        
-    if movie["maturity"] != "no":
-        maturitycode = maturitycoder(moviedict["maturity"])
-    if movie["length"] != "no":
-        length = movie["length"].strip().lower()
-        if movie["length"] == "long" or movie["length"] == "longer":
+    
+    if moviedict["length"] != "no":
+        length = moviedict["length"].strip().lower()
+        if moviedict["length"] == "long" or moviedict["length"] == "longer":
             moviecode = 2
-        if movie["length"] == "medium":
+        if moviedict["length"] == "medium":
             moviecode = 1
-        if movie["length"] == "short" or movie["length"] =="shorter":
+        if moviedict["length"] == "short" or moviedict["length"] =="shorter":
             moviecode = 0
-    #TODO if statement, check if they input anything for the "anything else?" and the how many movies do u want outputted, if not assign default
+        else:
+            moviecode = 1
+    
+    vector = [decadecode,moviecode,genreforvector,nlpout[1]]
+    
+    listofsimilarities = []
+    for index in range(1000):
+        listofsimilarities.append(calculate_score(df.loc[index,"Vector"], vector))
+        print(df.loc[index,"Titles"])
+        
+    listofsimilarities.sort(reverse=True)
+    print(listofsimilarities)
+        
+def nlp(text):
+    taglist = ['absurd', 'action', 'adult comedy', 'allegory', 'alternate history', 'alternate reality', 'anti war', 'atmospheric', 'autobiographical', 'avant garde', 'blaxploitation', 'bleak', 'boring', 'brainwashing', 'claustrophobic', 'clever', 'comedy', 'comic', 'cruelty', 'cult', 'cute', 'dark', 'depressing', 'dramatic', 'entertaining', 'fantasy', 'feel-good', 'flashback', 'good versus evil', 'gothic', 'haunting', 'historical', 'historical fiction', 'home movie', 'horror', 'humor', 'insanity', 'inspiring', 'intrigue', 'magical realism', 'melodrama', 'murder', 'mystery', 'neo noir', 'non fiction', 'paranormal', 'philosophical', 'plot twist', 'pornographic', 'prank', 'psychedelic', 'psychological', 'queer', 'realism', 'revenge', 'romantic', 'sadist', 'satire', 'sci-fi', 'sentimental', 'storytelling', 'stupid', 'suicidal', 'suspenseful', 'thought-provoking', 'tragedy', 'violence', 'western', 'whimsical']
+    g = get_genre_prediction(text)
+    t = get_tags(text)
+    tag_vector = [0]*len(taglist)
+    for index,value in enumerate(taglist):
+        if value in t:
+            tag_vector[index] = 1
+    return(g,tag_vector)
 
 
 
-#def constructvector(decade, maturity, length, genre, tag):
-
-dd = {"similar_movies": "lala", "genres": "Comedy , horror,adventure", "decade": "1956"}
+dd = {"similar_movies": "lala", "genres": "Comedy , horror,adventure", "decade": "1956", "textbox": "im feeling so happy", "length": "long"}
 take_input(dd)
---
+++ vector_score.py
@@ -0,0 +1,59 @@
+
+import pandas
+import numpy as np
+#this script is run on a constrained frame, with only the requested pg 13 
+
+    
+def calculate_similarity(a, b):
+      return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+df = pandas.read_csv("movie_data.csv")
+def strtovector(uservector):
+    lista = []
+
+
+    lista.append(int(uservector[1]))
+   
+
+    lista.append(int(uservector[4]))
+   
+
+    listagenre = []
+    for i in range(8,68,3):
+        listagenre.append(int(uservector[i]))
+    
+    listatag = []
+    for i in range(70,277,3):
+        if int(uservector[i]) == 0:
+            listatag.append(int(uservector[i]))
+        else:
+            listatag.append(15)
+    lista.append(listagenre)
+    lista.append(listatag)
+  
+    return(lista)
+
+def calculate_score(movievector,uservector):
+    v = strtovector(movievector)
+    
+    va = []
+    vb = []
+    va.append(v[0])
+    va.append(v[1])
+    vb.append(uservector[0])
+    vb.append(uservector[1])
+    for n in v[2]:
+        va.append(n)
+    for m in v[3]:
+        va.append(m)
+    for p in uservector[2]:
+        vb.append(p)
+    for q in uservector[3]:
+        vb.append(q)
+
+    
+    
+    return(calculate_similarity(va,vb))
+
+
+
