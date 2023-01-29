b68e0b16206c3efb52d45a3547728a3eac07914e
Rylan Donohoe <rylan.donohoe@outlook.com>
2023-01-29 02:58:39 -0500

    update

---
+++ coding.py
@@ -0,0 +1,76 @@
+def decade_coder(year):
+    year = year[:-1]
+    if year == "192":
+        return 0
+    elif year == "193":
+        return 1
+    elif year == "194":
+        return 2
+    elif year == "195":
+        return 3
+    elif year == "196":
+        return 4
+    elif year == "197":
+        return 5
+    elif year == "198":
+        return 6
+    elif year == "199":
+        return 7
+    elif year == "200":
+        return 8
+    elif year == "201":
+        return 9
+    elif year == "202":
+        return 10
+    else:
+        return 1000
+
+'''
+def maturity_coder(maturity):
+    maturity.lower().strip()
+    if maturity == "g":
+        return 0
+    elif maturity == "pg":
+        return 1
+    elif maturity == "pg-13" or maturity =="pg13":
+        return 2
+    elif maturity == "r":
+        return 3
+    else:
+        return 1000
+'''
+
+def length_coder(length):
+    length = int(length)
+    if length < 60:
+        return 0
+    elif 60 <= length <= 120:
+        return 1
+    elif length > 120:
+        return 2
+    else:
+        return 1000
+
+def genre_encoding(str_of_genres):
+    master = ['action', 'adventure', 'animation', 'biography', 'comedy', 'crime', 'drama', 'family', 'fantasy', 'history', 'horror', 'music', 'musical', 'mystery', 'romance', 'sci-fi', 'sport', 'thriller', 'war', 'western']
+    list_of_genres = str_of_genres.split(",")
+    processed_list = []
+    for i in list_of_genres:
+        processed_list.append(i.strip().lower())
+    genre_vector = [0]*len(master)
+    for index, genre in enumerate(master):
+        if genre in processed_list:
+            genre_vector[index] = 1
+    return genre_vector
+
+def tag_encoding(str_of_tags):
+    master = ['absurd', 'action', 'adult comedy', 'allegory', 'alternate history', 'alternate reality', 'anti war', 'atmospheric', 'autobiographical', 'avant garde', 'blaxploitation', 'bleak', 'boring', 'brainwashing', 'claustrophobic', 'clever', 'comedy', 'comic', 'cruelty', 'cult', 'cute', 'dark', 'depressing', 'dramatic', 'entertaining', 'fantasy', 'feel-good', 'flashback', 'good versus evil', 'gothic', 'haunting', 'historical', 'historical fiction', 'home movie', 'horror', 'humor', 'insanity', 'inspiring', 'intrigue', 'magical realism', 'melodrama', 'murder', 'mystery', 'neo noir', 'non fiction', 'paranormal', 'philosophical', 'plot twist', 'pornographic', 'prank', 'psychedelic', 'psychological', 'queer', 'realism', 'revenge', 'romantic', 'sadist', 'satire', 'sci-fi', 'sentimental', 'storytelling', 'stupid', 'suicidal', 'suspenseful', 'thought-provoking', 'tragedy', 'violence', 'western', 'whimsical']
+    list_of_tags = str_of_tags.split(",")
+    processed_list = []
+    for i in list_of_tags:
+        processed_list.append(i.strip().lower())
+    tag_vector = [0]*len(master)
+    for index, tag in enumerate(master):
+        if tag in processed_list:
+            tag_vector[index] = 1
+    return tag_vector
--- df_creation.py
+++ df_creation.py
@@ -3,4 +3,128 @@ import pandas as pd
 df1 = pd.read_csv("top_1000.csv")
 df2 = pd.read_csv("mpst.csv")
 
-print(df1.to_string())
+df1 = df1.drop(['Metascore'], axis=1)
+
+# titles list
+def gen_titles_list():
+    titles_list = []
+    for i in range(1000):
+        title = df1.loc[i,"Title"].lstrip(" .0123456789")[:-7]
+        titles_list.append(title)
+    return titles_list
+
+# years list
+def gen_years_list():
+    years_list = []
+    for i in range(1000):
+        year = df1.loc[i,"Title"][-5:][:-1]
+        years_list.append(year)
+    return years_list
+
+# durations list
+def gen_durations_list():
+    durations_list = []
+    for i in range(1000):
+        duration = df1.loc[i,"Duration"][:-4]
+        durations_list.append(duration)
+    return durations_list
+
+# genres list
+def gen_genres_list():
+    genres_list = []
+    for i in range(1000):
+        genres = df1.loc[i,"Genre"]
+        genres_list.append(genres.lower())
+    for i in range(1000):
+        if 'film-noir' in genres_list[i]:
+            genres_list[i] = genres_list[i].replace('film-noir','').strip(" ,")
+    return genres_list
+
+genres_list = gen_genres_list()
+
+def gen_unique_genres():
+    unique_genres = []
+    for i in range(1000):
+        current_genres_tags = genres_list[i].split(',')
+        for j in current_genres_tags:
+            if j.strip() not in unique_genres:
+                unique_genres.append(j.strip())
+    unique_genres.sort()
+    return unique_genres
+
+# tags list
+titles_list = gen_titles_list()
+
+def gen_tags_list():
+    tags_list = []
+    for i in titles_list:
+        tags = df2.loc[df2["title"] == i]["tags"].tolist()
+        try:
+            tags = tags[0].strip('\'')
+        except:
+            tags = ' '
+        tags_list.append(tags)
+    return tags_list
+
+tags_list = gen_tags_list()
+
+def gen_unique_tags():
+    unique_tags = []
+    for i in range(1000):
+        if tags_list[i] == '':
+            continue
+        current_unique_tags = tags_list[i].split(',')
+        for j in current_unique_tags:
+            if j.strip() not in unique_tags:
+                unique_tags.append(j.strip())
+    unique_tags.sort()
+    return unique_tags
+
+# descriptions list
+def gen_descriptions_list():
+    descriptions_list = []
+    for i in range(1000):
+        description = df1.loc[i,"Description"]
+        descriptions_list.append(description)
+    return descriptions_list
+
+# certificates list
+def gen_certificates_list():
+    certificates_list = []
+    for i in range(1000):
+        certificate = df1.loc[i,"Certificate"]
+        certificates_list.append(certificate)
+    for i in range(1000):
+        if str(certificates_list[i]) in ['Approved','Not Rated','Passed','TV-MA','nan']:
+            certificates_list[i] = 'R'
+        elif certificates_list[i] in ['TV-PG','GP']:
+            certificates_list[i] = 'PG'
+        elif certificates_list[i] == 'TV-14':
+            certificates_list[i] = 'PG-13'
+    return certificates_list
+
+# directors list
+def gen_directors_list():
+    directors_list = []
+    for i in range(1000):
+        bar = df1.loc[i,"Cast"].rfind('|')
+        if df1.loc[i,"Cast"][8] == 's':
+            director = df1.loc[i,"Cast"][11:bar-1]
+        else:
+            director = df1.loc[i,"Cast"][10:bar-1]
+        directors_list.append(director)
+    return directors_list
+
+# actors list
+def gen_actors_list():
+    actors_list = []
+    for i in range(1000):
+        bar = df1.loc[i,"Cast"].rfind('|')
+        actors = df1.loc[i,"Cast"][bar+9:]
+        actors_list.append(actors)
+    return actors_list
+
+# create dataframe
+df3 = pd.DataFrame({'Titles': gen_titles_list(),'Year': gen_years_list(),'Duration': gen_durations_list(),'Genre(s)': gen_genres_list(),'Tag(s)': gen_tags_list(),'Synopsis': gen_descriptions_list(),'Age Rating': gen_certificates_list(),'Director(s)': gen_directors_list(),'Star(s)': gen_actors_list()})
+
+df3.to_csv('movie_data.csv')
